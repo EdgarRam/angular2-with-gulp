742955c94e5a62029c32e59eb261ca5e6feb0ee7
Edgar Ramirez <tanotuff12@gmail.com>
2017-03-14 15:45:05 -0600

    feat(): se agrega gulp task dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var config = require('./build-helpers/config');
 config.tscConfig = require('./tsconfig.json');
 config.fn.readFolder( './tasks' );
 
-config.modules.gulp.task('default', config.modules.sync.sync(
+config.modules.gulp.task('default', function(){
+	console.log("no se puede hacer nada en default!!!");
+})
+
+config.modules.gulp.task('dev', config.modules.sync.sync(
 	[
 		'clean',
         [
